46db150c0fabcfb9c4682221648bb52104fe95ec
unknown <za0162hid@gmail.com>
2024-02-15 18:24:03 +0600

    office education display

--- src/Components/Pages/DisplayProductsPages/DisplayProductCategories/OfficeEducationDisplay.js
+++ src/Components/Pages/DisplayProductsPages/DisplayProductCategories/OfficeEducationDisplay.js
@@ -12,7 +12,7 @@ const OfficeEducationDisplay = () => {
 
   useEffect(() => {
     const storedTab = localStorage.getItem("officeEducationActiveTab");
-    setOfficeEducationActiveTab(storedTab || "#-tab"); // Setting default active tab
+    setOfficeEducationActiveTab(storedTab || "#Paper_Products-tab"); // Setting default active tab
   }, []);
 
   const handleTabChange = (tab) => {
@@ -33,21 +33,127 @@ const OfficeEducationDisplay = () => {
             <span>Office And Education</span>
           </section>
           <button
-            id="-tab"
+            id="Paper_Products-tab"
             data-bs-toggle="pill"
-            data-bs-target="#"
+            data-bs-target="#Paper_Products"
             type="button"
             role="tab"
-            aria-controls=""
+            aria-controls="Paper_Products"
             aria-selected={
-              officeEducationActiveTab === "#-tab" ? "true" : "false"
+              officeEducationActiveTab === "#Paper_Products-tab"
+                ? "true"
+                : "false"
             }
             className={`nav-link categoryButton ${
-              officeEducationActiveTab === "#-tab" ? "active" : ""
+              officeEducationActiveTab === "#Paper_Products-tab" ? "active" : ""
             }`}
-            onClick={() => handleTabChange("#-tab")}
+            onClick={() => handleTabChange("#Paper_Products-tab")}
           >
-            Novelty Gifts
+            Paper Products
+          </button>
+
+          <button
+            id="Writing_Correction-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#Writing_Correction-tab"
+            type="button"
+            role="tab"
+            aria-controls="Writing_Correction"
+            aria-selected={
+              officeEducationActiveTab === "#Writing_Correction-tab"
+                ? "true"
+                : "false"
+            }
+            className={`nav-link categoryButton ${
+              officeEducationActiveTab === "#Writing_Correction-tab"
+                ? "active"
+                : ""
+            }`}
+            onClick={() => handleTabChange("#Writing_Correction-tab")}
+          >
+            Writing and Correction
+          </button>
+
+          <button
+            id="Files_Folders-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#Files_Folders"
+            type="button"
+            role="tab"
+            aria-controls="Files_Folders"
+            aria-selected={
+              officeEducationActiveTab === "#Files_Folders-tab"
+                ? "true"
+                : "false"
+            }
+            className={`nav-link categoryButton ${
+              officeEducationActiveTab === "#Files_Folders-tab" ? "active" : ""
+            }`}
+            onClick={() => handleTabChange("#Files_Folders-tab")}
+          >
+            Files and Folders
+          </button>
+
+          <button
+            id="Desk_Accessories-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#Desk_Accessories"
+            type="button"
+            role="tab"
+            aria-controls="Desk_Accessories"
+            aria-selected={
+              officeEducationActiveTab === "#Desk_Accessories-tab"
+                ? "true"
+                : "false"
+            }
+            className={`nav-link categoryButton ${
+              officeEducationActiveTab === "#Desk_Accessories-tab"
+                ? "active"
+                : ""
+            }`}
+            onClick={() => handleTabChange("#Desk_Accessories-tab")}
+          >
+            Desk Accessories
+          </button>
+
+          <button
+            id="Books-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#Books"
+            type="button"
+            role="tab"
+            aria-controls="Books"
+            aria-selected={
+              officeEducationActiveTab === "#Books-tab" ? "true" : "false"
+            }
+            className={`nav-link categoryButton ${
+              officeEducationActiveTab === "#Books-tab" ? "active" : ""
+            }`}
+            onClick={() => handleTabChange("#Books-tab")}
+          >
+            Books
+          </button>
+
+          <button
+            id="Others_Stationeries-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#Others_Stationeries"
+            type="button"
+            role="tab"
+            aria-controls="Others_Stationeries"
+            aria-selected={
+              officeEducationActiveTab === "#Others_Stationeries-tab"
+                ? "true"
+                : "false"
+            }
+            className={`nav-link categoryButton ${
+              officeEducationActiveTab === "#Others_Stationeries-tab"
+                ? "active"
+                : ""
+            }`}
+            onClick={() => handleTabChange("#Others_Stationeries-tab")}
+          >
+            Others Stationeries
           </button>
         </aside>
         {/* Sidebar end */}
@@ -57,13 +163,15 @@ const OfficeEducationDisplay = () => {
           className="tab-content me-2 categoryContentDisplay"
           id="v-pills-tabContent"
         >
-          {/* Novelty */}
+          {/* Paper Products */}
           <div
-            id=""
+            id="Paper_Products"
             role="tabpanel"
-            aria-labelledby="-tab"
+            aria-labelledby="Paper_Products-tab"
             className={`tab-pane fade ${
-              officeEducationActiveTab === "#-tab" ? "show active" : ""
+              officeEducationActiveTab === "#Paper_Products-tab"
+                ? "show active"
+                : ""
             }`}
           >
             <section className="row tab-content-body">
@@ -77,7 +185,20 @@ const OfficeEducationDisplay = () => {
               </section>
               <Link
                 className="col-12 col-lg-2 col-md-2 categoryContentBody"
-                to={`/displayProduct/${""}`}
+                to={`/displayProduct/${"Diary"}`}
+              >
+                <LazyLoadImage
+                  src=""
+                  alt="Trulli"
+                  className="img-fluid categoryImage"
+                  placeholderSrc={loader}
+                />
+                <figcaption className="categoryImageCaption">Diary</figcaption>
+              </Link>
+
+              <Link
+                className="col-12 col-lg-2 col-md-2 categoryContentBody"
+                to={`/displayProduct/${"Notebooks and Pads"}`}
               >
                 <LazyLoadImage
                   src=""
@@ -86,7 +207,266 @@ const OfficeEducationDisplay = () => {
                   placeholderSrc={loader}
                 />
                 <figcaption className="categoryImageCaption">
-                  Novelty Gifts
+                  Notebooks and Pads
+                </figcaption>
+              </Link>
+
+              <Link
+                className="col-12 col-lg-2 col-md-2 categoryContentBody"
+                to={`/displayProduct/${"Writing and Printing Papers"}`}
+              >
+                <LazyLoadImage
+                  src=""
+                  alt="Trulli"
+                  className="img-fluid categoryImage"
+                  placeholderSrc={loader}
+                />
+                <figcaption className="categoryImageCaption">
+                  Writing and Printing Papers
+                </figcaption>
+              </Link>
+
+              <Link
+                className="col-12 col-lg-2 col-md-2 categoryContentBody"
+                to={`/displayProduct/${"Envelops & Mailers"}`}
+              >
+                <LazyLoadImage
+                  src=""
+                  alt="Trulli"
+                  className="img-fluid categoryImage"
+                  placeholderSrc={loader}
+                />
+                <figcaption className="categoryImageCaption">
+                  Envelops & Mailers
+                </figcaption>
+              </Link>
+
+              <Link
+                className="col-12 col-lg-2 col-md-2 categoryContentBody"
+                to={`/displayProduct/${"Calendars"}`}
+              >
+                <LazyLoadImage
+                  src=""
+                  alt="Trulli"
+                  className="img-fluid categoryImage"
+                  placeholderSrc={loader}
+                />
+                <figcaption className="categoryImageCaption">
+                  Calendars
+                </figcaption>
+              </Link>
+
+              <Link
+                className="col-12 col-lg-2 col-md-2 categoryContentBody"
+                to={`/displayProduct/${"PaperProductsOthers"}`}
+              >
+                <LazyLoadImage
+                  src=""
+                  alt="Trulli"
+                  className="img-fluid categoryImage"
+                  placeholderSrc={loader}
+                />
+                <figcaption className="categoryImageCaption">
+                  PaperProductsOthers
+                </figcaption>
+              </Link>
+            </section>
+          </div>
+
+          {/* Writing & Correction */}
+          <div
+            id="Writing_Correction"
+            role="tabpanel"
+            aria-labelledby="Writing_Correction-tab"
+            className={`tab-pane fade ${
+              officeEducationActiveTab === "#Writing_Correction-tab"
+                ? "show active"
+                : ""
+            }`}
+          >
+            <section className="row tab-content-body">
+              <section className="d-flex justify-content-center my-3 tab-content-head">
+                <LazyLoadImage
+                  src="https://res.cloudinary.com/shokhbari/image/upload/v1705565985/01-Novelty-Gifts_hnrm3w.png"
+                  alt=""
+                  className="img-fluid subCateBanner"
+                  effect="blur"
+                />
+              </section>
+              <Link
+                className="col-12 col-lg-2 col-md-2 categoryContentBody"
+                to={`/displayProduct/${"Writing and Correction"}`}
+              >
+                <LazyLoadImage
+                  src=""
+                  alt="Trulli"
+                  className="img-fluid categoryImage"
+                  placeholderSrc={loader}
+                />
+                <figcaption className="categoryImageCaption">
+                  Writing and Correction
+                </figcaption>
+              </Link>
+            </section>
+          </div>
+
+          {/* Files and Folders */}
+          <div
+            id="Files_Folders"
+            role="tabpanel"
+            aria-labelledby="Files_Folders-tab"
+            className={`tab-pane fade ${
+              officeEducationActiveTab === "#Files_Folders-tab"
+                ? "show active"
+                : ""
+            }`}
+          >
+            <section className="row tab-content-body">
+              <section className="d-flex justify-content-center my-3 tab-content-head">
+                <LazyLoadImage
+                  src="https://res.cloudinary.com/shokhbari/image/upload/v1705565985/01-Novelty-Gifts_hnrm3w.png"
+                  alt=""
+                  className="img-fluid subCateBanner"
+                  effect="blur"
+                />
+              </section>
+              <Link
+                className="col-12 col-lg-2 col-md-2 categoryContentBody"
+                to={`/displayProduct/${"Files and Folders"}`}
+              >
+                <LazyLoadImage
+                  src=""
+                  alt="Trulli"
+                  className="img-fluid categoryImage"
+                  placeholderSrc={loader}
+                />
+                <figcaption className="categoryImageCaption">
+                  Files and Folders
+                </figcaption>
+              </Link>
+            </section>
+          </div>
+
+          {/* Desk Accessories */}
+          <div
+            id="Desk_Accessories"
+            role="tabpanel"
+            aria-labelledby="Desk_Accessories-tab"
+            className={`tab-pane fade ${
+              officeEducationActiveTab === "#Desk_Accessories-tab"
+                ? "show active"
+                : ""
+            }`}
+          >
+            <section className="row tab-content-body">
+              <section className="d-flex justify-content-center my-3 tab-content-head">
+                <LazyLoadImage
+                  src="https://res.cloudinary.com/shokhbari/image/upload/v1705565985/01-Novelty-Gifts_hnrm3w.png"
+                  alt=""
+                  className="img-fluid subCateBanner"
+                  effect="blur"
+                />
+              </section>
+
+              <Link
+                className="col-12 col-lg-2 col-md-2 categoryContentBody"
+                to={`/displayProduct/${"Desk Accessories"}`}
+              >
+                <LazyLoadImage
+                  src=""
+                  alt="Trulli"
+                  className="img-fluid categoryImage"
+                  placeholderSrc={loader}
+                />
+                <figcaption className="categoryImageCaption">
+                  Desk Accessories
+                </figcaption>
+              </Link>
+            </section>
+          </div>
+
+          {/* Books */}
+          <div
+            id="Books"
+            role="tabpanel"
+            aria-labelledby="Books-tab"
+            className={`tab-pane fade ${
+              officeEducationActiveTab === "#Books-tab" ? "show active" : ""
+            }`}
+          >
+            <section className="row tab-content-body">
+              <section className="d-flex justify-content-center my-3 tab-content-head">
+                <LazyLoadImage
+                  src="https://res.cloudinary.com/shokhbari/image/upload/v1705565985/01-Novelty-Gifts_hnrm3w.png"
+                  alt=""
+                  className="img-fluid subCateBanner"
+                  effect="blur"
+                />
+              </section>
+              <Link
+                className="col-12 col-lg-2 col-md-2 categoryContentBody"
+                to={`/displayProduct/${"Academic"}`}
+              >
+                <LazyLoadImage
+                  src=""
+                  alt="Trulli"
+                  className="img-fluid categoryImage"
+                  placeholderSrc={loader}
+                />
+                <figcaption className="categoryImageCaption">
+                  Academic
+                </figcaption>
+              </Link>
+
+              <Link
+                className="col-12 col-lg-2 col-md-2 categoryContentBody"
+                to={`/displayProduct/${"Non-Academic"}`}
+              >
+                <LazyLoadImage
+                  src=""
+                  alt="Trulli"
+                  className="img-fluid categoryImage"
+                  placeholderSrc={loader}
+                />
+                <figcaption className="categoryImageCaption">
+                  Non-Academic
+                </figcaption>
+              </Link>
+            </section>
+          </div>
+
+          {/* Other Sationeries */}
+          <div
+            id="Others_Stationeries"
+            role="tabpanel"
+            aria-labelledby="Others_Stationeries-tab"
+            className={`tab-pane fade ${
+              officeEducationActiveTab === "#Others_Stationeries-tab"
+                ? "show active"
+                : ""
+            }`}
+          >
+            <section className="row tab-content-body">
+              <section className="d-flex justify-content-center my-3 tab-content-head">
+                <LazyLoadImage
+                  src="https://res.cloudinary.com/shokhbari/image/upload/v1705565985/01-Novelty-Gifts_hnrm3w.png"
+                  alt=""
+                  className="img-fluid subCateBanner"
+                  effect="blur"
+                />
+              </section>
+              <Link
+                className="col-12 col-lg-2 col-md-2 categoryContentBody"
+                to={`/displayProduct/${"Others Stationeries"}`}
+              >
+                <LazyLoadImage
+                  src=""
+                  alt="Trulli"
+                  className="img-fluid categoryImage"
+                  placeholderSrc={loader}
+                />
+                <figcaption className="categoryImageCaption">
+                  Others Stationeries
                 </figcaption>
               </Link>
             </section>
