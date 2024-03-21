f99daf5dde66d1d60463c0e55bbb7c64a44af977
unknown <za0162hid@gmail.com>
2024-03-21 13:59:39 +0600

    kids segments

--- src/Components/Pages/DisplayProductsPages/DisplayProductCategories/KidsSegmentDisplay.js
+++ src/Components/Pages/DisplayProductsPages/DisplayProductCategories/KidsSegmentDisplay.js
@@ -29,7 +29,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"Infants_cloth"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007364/Infants-Cloth-5_c7fuh3.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -44,7 +44,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"Boy_cloth"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007359/Boy-Cloth-1_zfsgua.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -59,7 +59,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"Girl_cloth"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007361/Girls-Cloth-4_fo2ohf.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -87,7 +87,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"Infants_Footwaer"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007515/Infants-Footwaer-4_idh2xt.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -102,7 +102,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"Boy_Footwaer"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007519/Boy-Footwaer-2_gpzskx.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -117,7 +117,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"Girl_Footwaer"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007512/Girl-Footwaer-1_br4d5o.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -145,7 +145,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"Baby_body_care"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007621/Kids-Body-Care-3_quc1ta.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -160,7 +160,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"Baby_PersonalCare"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007625/Kids-Personal-Care-1_jm5rco.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -188,7 +188,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"Infants_Accessories"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007708/Infants-Accessories-4_hevtlr.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -203,7 +203,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"Boy_Accessories"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007705/Boy_Accessories_3_qkmagj.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -218,7 +218,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"Girl_Accessories"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007701/girl-Accessories-4_jcikqw.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -246,7 +246,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"BabyFeedingNursery"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007818/Kids_Feeding_Nursery_3_aqtghv.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -274,7 +274,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"BabySportsOutdoor"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007826/Kids-Sports-Outdoor-1_omdk4t.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -302,7 +302,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"BabyToysGames"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007830/Kids-Toys-Games-3_c8rdua.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
@@ -331,7 +331,7 @@ const KidsSegmentDisplay = () => {
                   to={`/displayProduct/${"BabyEducation"}`}
                 >
                   <LazyLoadImage
-                    src=""
+                    src="https://res.cloudinary.com/shokhbari/image/upload/v1711007822/Kids-Education-555_oiewy2.jpg"
                     alt="Trulli"
                     className="img-fluid categoryImage"
                     placeholderSrc={loader}
